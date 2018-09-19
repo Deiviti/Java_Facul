39e17871d8065e7f860490f10655927476926118
deivi <deivi@192.168.25.149>
2018-09-19 17:24:10 -0300

    atualiza trabalho 02

--- Agenda/src/ViewCadContato.java
+++ Agenda/src/ViewCadContato.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 
 public class ViewCadContato {
 	
-	private Scanner input;
-	
+	Scanner teclado = new Scanner(System.in);
+	private ArrayList<Contato> listaCadContato;
 	public ViewCadContato() {
 		setInput(new Scanner(System.in));
 	}
 	
-	public String mostraMenu() {
+	public String mostraMenu() {		
+		System.out.println("\n");
 		System.out.println("Agenda de Contatos");
+		System.out.println("\n");
 		System.out.println("1 - Inserir");
 		System.out.println("2 - Listar");
 		System.out.println("3 - Alterar");
@@ -19,29 +21,27 @@ public class ViewCadContato {
 		System.out.println("6 - Sair");
 		System.out.println("Opção: ");
 		System.out.println("\n");
-		return input.nextLine();
+		return teclado.nextLine();
 	}
 	
 	public Contato inserir(ArrayList<Contato> listaCadContato) {
 
 		Contato contato = new Contato();
-
 		System.out.println("Digite o Nome do Contato: \n");
-		contato.setNome(input.nextLine());
-		
+		contato.setNome(teclado.nextLine());		
 		System.out.println("Digite a Idade do Contato: \n");
-		contato.setIdade(input.nextLine());
-		
+		contato.setIdade(teclado.nextLine());		
 		System.out.println("Digite o telefone do Contato: \n");
-		contato.setTelefone(input.nextLine());
-
-		return contato;
+		contato.setTelefone(teclado.nextLine());		
+		listaCadContato.add(contato);		
+		return contato;		
 	}
 	
 	public void listar(ArrayList<Contato> listaCadContato ) {
-		for (int x = 0; x < listaCadContato.size(); x++) {
-			System.out.println(listaCadContato.get(x).toString() + "Posição: %d - $s" + x);
-			System.out.println("\n");
+		int ct = listaCadContato.size();
+		for (int x = 0; x < ct; x++) {
+			System.out.printf("Código Contato %d - %s\n", x+1, listaCadContato.get(x));
+			System.out.println("\n");			
 		}
 	}
 	
@@ -52,7 +52,8 @@ public class ViewCadContato {
 		do {
 			System.out.println("Digite o código do contato para alterar: ");
 			try {
-				x = Integer.parseInt(input.nextLine());
+				x = Integer.parseInt(teclado.nextLine());
+				
 				if (x >= 0 & x < listaCadContato.size()) {
 					continuar = false;
 				} else {
@@ -64,13 +65,13 @@ public class ViewCadContato {
 		} while (continuar);
 
 		System.out.println("Nome (" + listaCadContato.get(x).getNome() + "): ");
-		listaCadContato.get(x).setNome(input.nextLine());
+		listaCadContato.get(x).setNome(teclado.nextLine());
 
-		System.out.println("Marca(" + listaCadContato.get(x).getIdade() + "): ");
-		listaCadContato.get(x).setIdade(input.nextLine());
+		System.out.println("Idade(" + listaCadContato.get(x).getIdade() + "): ");
+		listaCadContato.get(x).setIdade(teclado.nextLine());
 
-		System.out.println("Modelo (" + listaCadContato.get(x).getTelefone() + "): ");
-		listaCadContato.get(x).setTelefone(input.nextLine());
+		System.out.println("Telefone (" + listaCadContato.get(x).getTelefone() + "): ");
+		listaCadContato.get(x).setTelefone(teclado.nextLine());
 
 	}
 
@@ -78,7 +79,7 @@ public class ViewCadContato {
 		listar(listaCadContato);
 		int x = 0;
 		System.out.println("Digite o código do contato para deletar");
-		x = Integer.parseInt(input.nextLine());
+		x = Integer.parseInt(teclado.nextLine());
 		listaCadContato.remove(x);
 	}
 
@@ -86,7 +87,7 @@ public class ViewCadContato {
 		boolean flag = true;
 		Contato contato = new Contato();
 		System.out.println("Digite o nome que deseja pesquisar: ");
-		contato.setNome(input.nextLine());
+		contato.setNome(teclado.nextLine());
 
 		for (int x = 0; x < listaCadContato.size(); x++) {
 			if (listaCadContato.get(x).getNome().equals(contato.getNome()) == true) {
@@ -106,14 +107,28 @@ public class ViewCadContato {
 	 * @return the input
 	 */
 	public Scanner getInput() {
-		return input;
+		return teclado;
 	}
 
 	/**
 	 * @param input the input to set
 	 */
 	public void setInput(Scanner input) {
-		this.input = input;
+		this.teclado = input;
+	}
+
+	/**
+	 * @return the listaCadContato
+	 */
+	public ArrayList<Contato> getListaCadContato() {
+		return listaCadContato;
+	}
+
+	/**
+	 * @param listaCadContato the listaCadContato to set
+	 */
+	public void setListaCadContato(ArrayList<Contato> listaCadContato) {
+		this.listaCadContato = listaCadContato;
 	}
 
 	
