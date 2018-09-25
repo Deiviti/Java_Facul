f0a1560630fd1bdea21f9a00e42b6bdd5adfecde
Deiviti Demis Paulino <200713157@unibrasil.com.br>
2018-09-25 20:31:54 -0300

    Aula Pacotes

---
+++ AulaPacotes/AulaPacotes/src/br/Principal.java
@@ -0,0 +1,22 @@
+package br;
+
+
+import br.com.unibrasil.*;
+import br.com.unibrasil.QualquerCoisa;
+
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new CoisaUm();
+		new CoisaDois();
+		new QualquerCoisa();
+	}
+
+}
---
+++ AulaPacotes/AulaPacotes/src/br/com/unibrasil/QualquerCoisa.java
@@ -0,0 +1,5 @@
+package br.com.unibrasil;
+
+public class QualquerCoisa {
+
+}
